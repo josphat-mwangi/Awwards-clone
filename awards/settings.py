c086badb253c61b118148fd961153dfe87f0e58c
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-18 07:01:45 +0300

    setup the database and timezone

--- awards/settings.py
+++ awards/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'award',
+        'USER': 'josphat',
+        'PASSWORD': 'nevergiveup',
+
     }
 }
 
@@ -104,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'Africa/Nairobi'
 
 TIME_ZONE = 'UTC'
 
