4b487b724601e33b563666713ff420e88eabf1c8
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-19 10:25:00 +0300

    add url for function in views.py

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+from .views import (PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView)
 
 urlpatterns = [
    path('', views.index, name='index'),
-   path('register', views.register, name='register')
+   path('register/', views.register, name='register'),
+   url(r'^profile/(\d)$', views.profile, name='profile'),
+   path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+   path('post/new/', PostCreateView.as_view(), name='post-create'),
+   path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+   path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
